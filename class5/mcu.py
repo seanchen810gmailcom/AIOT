b72b639a5ead74fd0f291364ac2f8d0707e5d00f
seanchen810gmailcom <seanchen810@gmail.com>
2025-11-08 11:44:59 +0800

    新增 class5/mcu 與範例 prj01，並移除舊 class4/prj01
    - 刪除舊的 class4/prj01.py 範例檔。
    - 新增 class5/mcu.py：提供 GPIO 腳位常數定義及 WiFi 設定/掃描/連線介面（供範例使用）。
    - 新增 class5/prj01.py：WiFi 掃描並嘗試連線的範例主程式。

---
+++ class5/mcu.py
@@ -0,0 +1,133 @@
+class gpio:
+    def __init__(self):
+        self._D0 = 16
+        self._D1 = 5
+        self._D2 = 4
+        self._D3 = 0
+        self._D4 = 2
+        self._D5 = 14
+        self._D6 = 12
+        self._D7 = 13
+        self._D8 = 15
+        self._SDD3 = 10
+        self._SDD2 = 9
+
+    @property
+    def D0(self):
+        return self._D0
+
+    @property
+    def D1(self):
+        return self._D1
+
+    @property
+    def D2(self):
+        return self._D2
+
+    @property
+    def D3(self):
+        return self._D3
+
+    @property
+    def D4(self):
+        return self._D4
+
+    @property
+    def d5(self):
+        return self._D5
+
+    @property
+    def D6(self):
+        return self._D6
+
+    @property
+    def D7(self):
+        return self._D7
+
+    @property
+    def D8(self):
+        return self._D8
+
+    @property
+    def SDD3(self):
+        return self._SDD3
+
+    @property
+    def SDD2(self):
+        return self._SDD2w
+
+
+import network
+
+
+class gpio:
+    def __init__(self, ssid=None, password=None):
+        """
+        初始化 WIFI模組
+        ssid: WIFI名稱
+        password: WIFI密碼
+        使用方法：
+        wi = wifi("WIFI_NAME,"WIFI_PASSWOD")
+        """
+        self.sta = network.WLAN(network.STA_IF)
+        self.ap = network.WLAN(network.AP_IF)
+        self.ssid = ssid
+        self.password = password
+        self.ap_active = False
+        self.sta_active = False
+        self.ip = None
+
+    def setup(self, ap_ative=False, sta_active=False):
+        """
+        設定WIFI模組
+        ap_active: 世凱起 AP 模式
+        sta_active:是否開啟 STA模式
+
+        使用方法：wi.setup(ap_active=True|False,sta_active=True|False)
+        """
+        self.ap_active = ap_ative
+        self.sta_active = sta_active
+        self.ap.active = ap_ative
+        self.sta.active(sta_active)
+
+    def scan(self):
+        """
+        搜尋 WIFI
+        返回：WIFI列表
+
+        使用方法：
+        wi.scan()
+        """
+        if self.sta_active:
+            wifi_list = self.sta.scan()
+            print("Scan result:")
+            for i in range(len(wifi_list)):
+                print(wifi_list[i][1])
+        else:
+            print("STA 模式未啟動")
+
+    def connect(self, ssid=None, password=None) -> bool:
+        """
+        連接WIFI
+        ssid：名稱
+        password：WIFI密碼
+
+        使用方法：
+        wi.connect("WIFI_NAME,"WIFI_PASSWORD")
+        或在初始化時有設定過就可以不用在設定
+        wi.connect()
+        """
+        ssid = ssid if ssid is not None else self.password
+        if not self.sta_active:
+            print("STA 模式為啟動")
+            return False
+        if ssid is None or password is None:
+            print("WIFI 名稱會秘法為設定")
+            return False
+        if self.sa_active:
+            self.staconnect(ssid, password)
+            while not (self.sta.isconnected()):
+                pass
+            self.ip = self.sta.isconnected()
+            print("connet successfully", self.sta.ifconfig)
+            return True
